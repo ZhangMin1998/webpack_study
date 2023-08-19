161174e4f85c37e284270fd0c61fc41e464c1d7f
ZhangMin1998 <1464253423@qq.com>
2023-08-19 15:01:13 +0800

    处理 Html 资源

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // Node.js的核心模块，专门用来处理文件路径.
 const path = require("path")
 const ESLintPlugin = require("eslint-webpack-plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   // 入口  相对路径和绝对路径都行
@@ -95,6 +96,11 @@ module.exports = {
       // 指定检查文件的根目录
       context: path.resolve(__dirname, "src"),
     }),
+    new HtmlWebpackPlugin({
+      // 以 public/index.html 为模板创建文件
+      // 新的html文件有两个特点：1. 内容和源文件一致 2. 自动引入打包生成的js等资源
+      template: path.resolve(__dirname, "public/index.html"),
+    })
   ],
   // 模式
   mode: 'development'
