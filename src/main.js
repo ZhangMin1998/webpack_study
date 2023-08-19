3a55876c5623bc0757cd821321c0295bd716a17a
ZhangMin1998 <1464253423@qq.com>
2023-08-19 21:08:16 +0800

    HotModuleReplacement

--- src/main.js
+++ src/main.js
@@ -7,5 +7,21 @@ import "./sass/index.sass"
 import "./sass/index.scss"
 import "./stylus/index.styl"
 
-console.log(count(5,3))
-console.log(sum(1,2,3))
+console.log(count(5,5))
+console.log(sum(1,2,314))
+
+// 判断是否支持HMR功能
+// 实际开发用比如：vue-loader, react-hot-loader
+if (module.hot) {
+  module.hot.accept('./js/count')
+  module.hot.accept('./js/sum')
+  // module.hot.accept('./js/count.js', function (count) {
+  //   const result1 = count(5,2)
+  //   console.log(result1)
+  // })
+
+  // module.hot.accept('./js/sum.js', function (sum) {
+  //   const result2 = sum(1,2,3)
+  //   console.log(result2)
+  // })
+}
